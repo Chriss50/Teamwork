37259d25b6ca4ab82e95f99a5c33372df8bf7e52
Chriss50 <ishic47@gmail.com>
2019-09-26 20:33:21 +0200

    ft(sign-in-api): User can signin, data are retrieved from a userdata model, a login validator added for the purpose of validating, a controller to help in geting data from the data storage, a route have been modified to add an endpoint for signin [Finishes #168790333]

--- server/routes/createuser.js
+++ server/routes/createuser.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import createUser from '../controllers/signupController';
+import login from '../controllers/loginController';
 
 const router = express.Router();
 
 router.post('/auth/signup', createUser);
+router.post('/auth/signin', login);
 
 module.exports = router;
 
