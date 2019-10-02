22ca1d922e14cedc6c27640e3da753b4117ba058
Chriss50 <ishic47@gmail.com>
2019-10-02 10:50:54 +0200

    ft(signup-form-homepage): User should see the signup form once he navigate to the site, I modified the create article page, modified the about page too, even the homepage[Finishes #168895432]

--- UI/js/like.js
+++ UI/js/like.js
@@ -1,3 +1,6 @@
 function myFunction(x) {
     x.classList.toggle("fa-flip-horizontal");
+    document.getElementById("myButton").onclick = function () {
+      location.href = "www.youtube.com";
+  };
   }
